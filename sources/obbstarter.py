ddc18f3359182ec8251471c5a17c31974e18054d
Jur van den Berg <jur@vdberg.io>
2024-03-24 15:00:36 +0100

    fix obb

--- sources/obbstarter.py
+++ sources/obbstarter.py
@@ -9,16 +9,22 @@ from sources import Source
 
 class ObbSource(Source):
     _resources = None
+    _obb_path: str
+    _filename: str
     _index = None
 
     def __init__(self, obb: str):
-        self._obb = ZipFile(obb, 'r')
+        self._obb_path = obb
+        obb = ZipFile(obb, 'r')
+        self.load_index(obb)
+        self._resources = {f.filename.split('/')[-1]: f.filename for f in obb.filelist if 'matrix' in f.filename}
+        self._filename = obb.filename
 
     def has_blob(self, blob: str) -> bool:
         return blob in self.resources()
 
     def get_blob(self, blob: str) -> bytes:
-        return self._obb.read(self.resources()[blob])
+        return ZipFile(self._obb_path, 'r').read(self.resources()[blob])
 
     def bundle_to_blob(self, bundle: str) -> Union[str, None]:
         try:
@@ -36,29 +42,23 @@ class ObbSource(Source):
         return None
 
     def index(self) -> Dict[str, Tuple[str, str, int]]:
-        if self._index is not None:
-            return self._index
+        return self._index
 
-        # msgpack.loads(UnityPy.load(primary_source.get_blob('index')).container['assets/buildtemp/index.bytes'].read().script)[0]
-        index_blob = self._obb.read('assets/resource/matrix/index')
+    def load_index(self, obb):
+        index_blob = obb.read('assets/resource/matrix/index')
         env = UnityPy.load(index_blob)
 
         if 'assets/buildtemp/index.bytes' not in env.container:
             raise Exception(f"Invalid OBB index bundle")
 
         self._index = msgpack.loads(env.container['assets/buildtemp/index.bytes'].read().script)[0]
-        return self._index
 
     def resources(self) -> Dict[str, str]:
-        if self._resources is not None:
-            return self._resources
-
-        self._resources = {f.filename.split('/')[-1]: f.filename for f in self._obb.filelist if 'matrix' in f.filename}
         return self._resources
 
     def bundle_names(self):
         return self.index().keys()
 
     def __str__(self):
-        return f"ObbSource({self._obb.filename})"
+        return f"ObbSource({self._filename})"
 
